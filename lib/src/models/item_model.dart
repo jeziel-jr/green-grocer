2783bb68093209b13d842817a2df9c0ec842d76a
Jeziel Junior <107517713+jeziel-jr@users.noreply.github.com>
2022-11-08 14:05:20 -0300

    first commit

---
+++ lib/src/models/item_model.dart
@@ -0,0 +1,15 @@
+class ItemModel {
+  ItemModel({
+    required this.itemName,
+    required this.imgUrl,
+    required this.unit,
+    required this.price,
+    required this.description,
+  });
+
+  String itemName;
+  String imgUrl;
+  String unit;
+  double price;
+  String description;
+}
---
+++ lib/src/pages/auth/components/custom_text_field.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    super.key,
+    required this.icon,
+    required this.label,
+    this.isSecret = false,
+    this.inputFormatters,
+  });
+
+  final IconData icon;
+  final String label;
+  final bool isSecret;
+  final List<TextInputFormatter>? inputFormatters;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool isObscure;
+
+  @override
+  void initState() {
+    super.initState();
+    isObscure = widget.isSecret;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: TextFormField(
+        inputFormatters: widget.inputFormatters,
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          prefixIcon: Icon(widget.icon),
+          suffixIcon: widget.isSecret
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isObscure = !isObscure;
+                    });
+                  },
+                  icon: isObscure
+                      ? const Icon(
+                          Icons.visibility,
+                          size: 25,
+                        )
+                      : const Icon(
+                          Icons.visibility_off,
+                          size: 25,
+                        ),
+                )
+              : null,
+          labelText: widget.label,
+          isDense: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+        ),
+      ),
+    );
+  }
+}
